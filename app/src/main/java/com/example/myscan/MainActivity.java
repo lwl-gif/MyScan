829a07699552ec13571cda9b3203c88d5150121d
lwl-gif <2495993430@qq.com>
2021-04-23 09:27:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myscan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/**
+ * @Author: Wallace
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
